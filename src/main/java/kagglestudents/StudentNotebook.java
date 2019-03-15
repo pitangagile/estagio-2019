5625986bdfee15bc55619537fbefbfd1de99a1e6
rlfo <rodolfo.oliveira@pitang.com>
2019-03-15 13:09:45 -0300

    kaggle students

---
+++ src/main/java/kagglestudents/StudentNotebook.java
@@ -0,0 +1,29 @@
+package kagglestudents;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class StudentNotebook {
+
+    private ExtractStudent extractStudent;
+    private List<StudentVO> studentNotebookList;
+
+    public StudentNotebook(ExtractStudent extractStudent) {
+        this.extractStudent = extractStudent;
+        this.studentNotebookList = extractStudent.load();
+    }
+
+    public Boolean isAnApprovedUser(Integer id) {
+
+        for (int i = 0; i < this.studentNotebookList.size(); i++) {
+            if(this.studentNotebookList.get(i).getId().equals(id)) {
+                return this.studentNotebookList.get(i).isApproved();
+            }
+        }
+
+        throw new NoSuchElementException("Nao foi encontrado nenhum elemento");
+
+
+
+    }
+}
