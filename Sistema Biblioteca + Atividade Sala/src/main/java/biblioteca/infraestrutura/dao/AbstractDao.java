78bdfc2a589492b0229acb1ea28e8c8d7ea9b793
Nycolas Lacerda <nycolas.lacerda@hotmail.com>
2019-03-26 16:23:47 -0300

    Add atividade DIAGRAMA ER

--- Sistema Biblioteca + Atividade Sala/src/main/java/biblioteca/infraestrutura/dao/AbstractDao.java
+++ Sistema Biblioteca + Atividade Sala/src/main/java/biblioteca/infraestrutura/dao/AbstractDao.java
@@ -4,8 +4,10 @@ import biblioteca.infraestrutura.IObjectPersistent;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.List;
@@ -48,9 +50,14 @@ public abstract class AbstractDao<T extends IObjectPersistent<C>,C> implements I
     @Override
     public List<T> findAll() {
 
-        NativeQuery query = this.getSession().createSQLQuery("select * from tblivro");
-
-        return query.list();
+          /*String ano = "1993";
+            select * from tb_livro as l where l.LIV_CL_ANOLANCAMENTO > :anolancamento (Maior que 93)
+            select * from tb_livro as l where l.LIV_CL_TITULO like '%guerra%'
+            select * from tb_livro as l where l.LIV_CL_SINOPSE like '%guerra%'
+            NativeQuery query = this.getSession().createSQLQuery("select * from tb_livro as l where l.LIV_CL_SINOPSE like '%guerra%' ");
+            query.setParameter("anolancamento",ano);
+            return query.list();*/
+          return null;
     }
 
     @Override
